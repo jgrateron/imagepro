9ab4e8a1bfb95947e0ea3bb4c00fb5aed8648893
jairo <jgrateron@gmail.com>
2020-07-23 16:51:01 -0400

    inicializacion de perfiles

--- src/main/java/com/fresco/imagepro/ImageproApplication.java
+++ src/main/java/com/fresco/imagepro/ImageproApplication.java
@@ -1,5 +1,8 @@
 package com.fresco.imagepro;
 
+import java.awt.color.ColorSpace;
+import java.awt.color.ICC_Profile;
+
 //creado por Jairo Grateron jgrateron@gmail.com
 
 import org.springframework.boot.SpringApplication;
@@ -10,7 +13,19 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class ImageproApplication {
 
+	/**
+	 * inicia los perfiles de colores
+	 */
+	public static void init ()
+	{
+        ICC_Profile.getInstance(ColorSpace.CS_sRGB).getData();
+        ICC_Profile.getInstance(ColorSpace.CS_PYCC).getData();
+        ICC_Profile.getInstance(ColorSpace.CS_GRAY).getData();
+        ICC_Profile.getInstance(ColorSpace.CS_CIEXYZ).getData();
+        ICC_Profile.getInstance(ColorSpace.CS_LINEAR_RGB).getData();		
+	}
 	public static void main(String[] args) {
+		init();
 		SpringApplication.run(ImageproApplication.class, args);
 	}
 
