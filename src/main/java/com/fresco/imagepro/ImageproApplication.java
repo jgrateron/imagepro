233cf99c5531bf58a9bbcee504b4a75253b4e260
jairo <jgrateron@gmail.com>
2020-07-23 17:11:23 -0400

    eliminacion init perfiles de colores

--- src/main/java/com/fresco/imagepro/ImageproApplication.java
+++ src/main/java/com/fresco/imagepro/ImageproApplication.java
@@ -1,7 +1,6 @@
 package com.fresco.imagepro;
 
-import java.awt.color.ColorSpace;
-import java.awt.color.ICC_Profile;
+
 
 //creado por Jairo Grateron jgrateron@gmail.com
 
@@ -13,19 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class ImageproApplication {
 
-	/**
-	 * inicia los perfiles de colores
-	 */
-	public static void init ()
-	{
-        ICC_Profile.getInstance(ColorSpace.CS_sRGB).getData();
-        ICC_Profile.getInstance(ColorSpace.CS_PYCC).getData();
-        ICC_Profile.getInstance(ColorSpace.CS_GRAY).getData();
-        ICC_Profile.getInstance(ColorSpace.CS_CIEXYZ).getData();
-        ICC_Profile.getInstance(ColorSpace.CS_LINEAR_RGB).getData();		
-	}
 	public static void main(String[] args) {
-		init();
 		SpringApplication.run(ImageproApplication.class, args);
 	}
 
